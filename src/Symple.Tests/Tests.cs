3e1cf66898e0af4c434c66d34cb5c5c97ae6efdb
Daniël Knippers <dknippers@gmail.com>
2024-08-19 19:21:54 +0200

    Add Test project with a few basic tests

---
+++ src/Symple.Tests/Tests.cs
@@ -0,0 +1,82 @@
+namespace Symple.Tests;
+
+public class Tests
+{
+    [Fact]
+    public void Should_Render_Template_Without_Special_Characters_Unchanged()
+    {
+        const string INPUT = @"Hello, World!";
+        const string EXPECTED = INPUT;
+        var output = Parser.Parse(INPUT).Render([]);
+        Assert.Equal(EXPECTED, output);
+    }
+
+    [Fact]
+    public void Should_Support_Simple_Variables()
+    {
+        const string INPUT = @"x = $x, y = $y";
+        const string EXPECTED = @"x = 1, y = 2";
+
+        var variables = new Dictionary<string, object>
+        {
+            ["x"] = 1,
+            ["y"] = 2
+        };
+
+        var output = Parser.Parse(INPUT).Render(variables);
+
+        Assert.Equal(EXPECTED, output);
+    }
+
+    [Fact]
+    public void Should_Support_Simple_If_Else_With_Variables()
+    {
+        const string INPUT = @"?[$condition]{$x}{$y} and ?[!$condition]{$x}{$y}";
+        const string EXPECTED = @"1 and 0";
+
+        var variables = new Dictionary<string, object>
+        {
+            ["condition"] = true,
+            ["x"] = 1,
+            ["y"] = 0,
+        };
+
+        var output = Parser.Parse(INPUT).Render(variables);
+
+        Assert.Equal(EXPECTED, output);
+    }
+
+    [Fact]
+    public void Should_Support_Nested_If_Else_With_Variables()
+    {
+        const string INPUT = @"?[$condition]{?[$x == ""hi""]{$y}{$x}}{0} and ?[!$condition]{1}{0}";
+        const string EXPECTED = @"hello and 0";
+
+        var variables = new Dictionary<string, object>
+        {
+            ["condition"] = true,
+            ["x"] = "hi",
+            ["y"] = "hello"
+        };
+
+        var output = Parser.Parse(INPUT).Render(variables);
+
+        Assert.Equal(EXPECTED, output);
+    }
+
+    [Fact]
+    public void Should_Support_Simple_Loop()
+    {
+        const string INPUT = @"@[$item : $items]{$item,}";
+        const string EXPECTED = @"1,2,3,4,5,6,7,8,9,10,";
+
+        var variables = new Dictionary<string, object>
+        {
+            ["items"] = Enumerable.Range(1, 10),
+        };
+
+        var output = Parser.Parse(INPUT).Render(variables);
+
+        Assert.Equal(EXPECTED, output);
+    }
+}
